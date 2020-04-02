ae1535739d9f9c803e885c5b326ebdba0bfe8879
mihadadene <mi.ihadadene@gmail.com>
2020-04-02 00:24:14 -0400

    first commit

---
+++ WcfServiceTax/IService1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceTax
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        double getTaxes(double montant);
+
+       
+       
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+
+    
+}
